5b4749f99bf6d96e20e44df82d951b8412f1faf7
Juan David Diaz <jdda886@gmail.com>
2024-05-14 17:28:33 -0400

    game loop fixed, extra images added

--- game.js
+++ game.js
@@ -1,81 +1,172 @@
 class Game {
-    constructor(){
-        this.startScreen = document.getElementById('game-intro')
-        this.gameScreen = document.getElementById("game-screen")
-        this.gameEndScreen = document.getElementById('game-end')
-        this.containers = document.getElementsByClassName('containers')
-        this.cat = document.getElementById('cat')
-        // this.player = new Player(this.gameScreen, 215, 450, 66, 150, '../images/shelby.png')
-        this.height = '85vw'
-        this.score = 0
-        this.lives = 3
-        this.timer = 60
-        this.gameIsOver = false
-        this.gameIntervalId = null
-        this.gameLoopFrequency = 1000/60
-        this.frames = 0
-        this.scoreElement = document.getElementById("score");
-        this.livesElement = document.getElementById("lives");
-        this.stats = document.getElementById("stats-container");
-        this.clockContainer = document.getElementById("clock-container");
-        this.clock = document.getElementById("clock");
-        this.endMessage = document.getElementById("end-message");
+  constructor() {
+    this.startScreen = document.getElementById("game-intro");
+    this.gameScreen = document.getElementById("game-screen");
+    this.gameEndScreen = document.getElementById("game-end");
+    this.containers = document.getElementsByClassName("containers");
+    this.cat = document.getElementById("cat");
+    // this.player = new Player(this.gameScreen, 215, 450, 66, 150, '../images/shelby.png')
+    this.height = "85vw";
+    this.score = 0;
+    this.lives = 3;
+    this.timer = 10;
+    this.gameIsOver = false;
+    this.gameIntervalId = null;
+    this.gameLoopFrequency = 1000 / 60;
+    this.frames = 0;
+    this.scoreElement = document.getElementById("score");
+    this.livesElement = document.getElementById("lives");
+    this.stats = document.getElementById("stats-container");
+    this.clockContainer = document.getElementById("clock-container");
+    this.clock = document.getElementById("clock");
+    this.winMessage = document.getElementById("win-message");
+    this.endMessage = document.getElementById("end-message");
+    this.catAttack = document.getElementById("cat-gif");
+    this.catWin = document.getElementById("cat-win");
+  }
+
+  start() {
+    this.gameScreen.style.height = `${this.height}px`;
+
+    this.cat.style.display = "block";
+
+    this.startScreen.style.display = "none";
+    this.startScreen.style.padding = 0;
+    this.startScreen.style.height = 0;
+
+    let theseContainers = [...this.containers];
+
+    theseContainers.forEach((container) => {
+      container.style.height = "fit-content";
+      container.style.display = "block";
+      console.log(this.lives);
+    });
+
+    this.gameIntervalId = setInterval(() => {
+      this.gameLoop();
+    }, this.gameLoopFrequency);
+  }
+
+  gameLoop() {
+    this.frames += 1;
+    this.update();
+
+    if (this.lives <= 0) {
+      console.log("Lives====>", this.lives);
+      this.gameIsOver = true;
     }
 
-    start() {
-        this.gameScreen.style.height = `${this.height}px`
-
-        this.cat.style.display = 'block'
-
-        this.startScreen.style.display = 'none'
-        this.startScreen.style.padding = 0
-        this.startScreen.style.height = 0
-
-        let theseContainers = [...this.containers]
-
-        theseContainers.forEach((container) => {
-            container.style.height = 'fit-content'
-            container.style.display = 'block'
-            console.log(this.lives )
-            
-        })
-
+    if (this.frames % 60 === 0) {
+      this.timer--;
+      this.clock.innerHTML = this.timer;
     }
 
-    gameLoop() {
-       
-
-
-        this.update()
-
-        if (this.lives <= 0) {
-            console.log("Lives====>", this.lives);
-            this.gameIsOver = true;
-          }
-        
-        if (this.timer === 0) {
-            this.timer --;
-            this.clock.innerHTML = this.timer;
-        }
-
-        if (this.timer <= 0) {
-            this.gameIsOver = true;
-          }
-
-        if (this.gameIsOver === true) {
-            clearInterval(this.gameIntervalId)
-            this.gameEndScreen();
-        }
+    if (this.timer === 0) {
+      this.clock.innerHTML = this.timer;
+      this.gameIsOver = true;
+      this.timer--;
     }
 
-    
+    if (this.timer <= 0) {
+      this.gameIsOver = true;
+    }
 
+    if (this.gameIsOver === true) {
+      clearInterval(this.gameIntervalId);
+      this.gameOverScreen();
+    }
+  }
 
+  update() {
+    this.livesElement.innerHTML = this.lives;
 
+    // this.clock.innerHTML = this.timer;
+  }
 
+  happyCat() {
+    console.log("Cat is happy function");
+    let happyCatSound = new Audio();
+    happyCatSound.src = "cat-purring.mp3";
+    happyCatSound.volume = 1;
+    happyCatSound.loop = true;
+    happyCatSound.play();
 
+    let timer = 0;
 
+    let happyInterval;
 
+    happyInterval = setInterval(() => {
+      timer++;
+      if (timer >= 4) {
+        happyCatSound.pause();
+        happyCatSound.volume = 0;
+        happyCatSound.loop = false;
+        clearInterval(happyInterval);
+      }
+    }, 500);
+  }
 
+  angryCat() {
+    console.log("Cat is angry function");
+    let angryCatSound = new Audio();
+    angryCatSound.src = "cat-hiss.mp3";
+    angryCatSound.volume = 1;
+    angryCatSound.loop = false;
+    angryCatSound.play();
 
+    let timer = 0;
+
+    let angryInterval;
+
+    angryInterval = setInterval(() => {
+      timer--;
+      if (timer >= 4) {
+        angryCat.pause();
+        angryCat.volume = 0;
+        angryCat.loop = false;
+        clearInterval(angryInterval);
+      }
+    }, 500);
+  }
+
+  superAngryCat() {
+    console.log("Cat is angry function");
+    let superAngryCatSound = new Audio();
+    superAngryCatSound.src = "cat-angry.mp3";
+    superAngryCatSound.volume = 1;
+    superAngryCatSound.loop = false;
+    superAngryCatSound.play();
+
+    let timer = 0;
+
+    let superAngryInterval;
+
+    superAngryInterval = setInterval(() => {
+      timer--;
+      if (timer >= 4) {
+        superAngryCat.pause();
+        superAngryCat.volume = 0;
+        superAngryCat.loop = false;
+        clearInterval(superAngryInterval);
+      }
+    }, 500);
+  }
+
+  gameOverScreen() {
+    console.log("Game is over!");
+    this.gameScreen.style.display = "none";
+    this.gameEndScreen.style.display = "inherit";
+
+    if (this.timer <= 0 && this.lives) {
+      this.catAttack.display = "none";
+      this.catWin.style.display = "inherit";
+      this.winMessage.innerText = `CONGRATULATIONS!`;
+      this.endMessage.innerHTML = `You won, with ${this.lives} lifes left you are clearly a cat person!`;
+    } else {
+      this.catWin.style.display = "none";
+      this.catAttack.style.display = "inherit";
+      this.winMessage.innerText = `GAME OVER`;
+      this.endMessage.innerHTML = ` You lost, grab some band-aids for you!`;
+    }
+  }
 }
--- scripts.js
+++ scripts.js
@@ -1,159 +1,175 @@
 window.onload = function () {
-    console.log("window is loaded")
-    const startButton = document.getElementById("start-button");
-    const restartButton = document.getElementById("restart-button");
+  console.log("window is loaded");
+  const startButton = document.getElementById("start-button");
+  const restartButton = document.getElementById("restart-button");
 
-    const catElement = document.getElementById('cat')
-  
-    let game
-  
-    function startGame() {
-      
-      game = new Game()
-      game.start()
-      
-      console.log("start game");
-       
-      
+  const catElement = document.getElementById("cat");
 
-    catElement.addEventListener('click', (e) => {
-        let catRect = catElement.getBoundingClientRect()
-        let catWidth = catRect.width
-        let catHeight = catRect.height
+  let game;
 
-        let catSectionWidth = catWidth/4
-        let catSectionHeight = catHeight/3
+  function startGame() {
+    game = new Game();
+    game.start();
 
+    console.log("start game");
+  }
 
-        if (e.offsetX >= 0 &&
-            e.offsetX <= catSectionWidth &&
-            e.offsetY >= 0 &&
-            e.offsetY <= catSectionHeight
-        ) {
-            console.log("Good spot - We're hitting the first of the top sections")
-        }
+  function restartGame() {
+    startGame();
 
-        if (e.offsetX >= catSectionWidth &&
-            e.offsetX <= catSectionWidth * 2 &&
-            e.offsetY >= 0 &&
-            e.offsetY <= catSectionHeight
-        ) {
-           
-            console.log("Bad spot - We're hitting the second of the top sections")
-        }
+    game.gameEndScreen.style.display = "none";
+    // game.gameEndScreen.style.padding = 0;
+    // game.gameEndScreen.style.height = 0;
 
-        if (e.offsetX >= catSectionWidth * 2 &&
-            e.offsetX <= catSectionWidth * 3 &&
-            e.offsetY >= 0 &&
-            e.offsetY <= catSectionHeight
-        ) {
-            console.log("Good spot - We're hitting the third of the top sections")
-        }
+    game.gameScreen.style.display = "inherit";
+  }
+  restartButton.addEventListener("click", () => {
+    console.log("clicking restart button");
+    restartGame();
+  });
+  startButton.addEventListener("click", () => {
+    console.log("Clicking Start Button");
+    startGame();
+  });
 
-        if (e.offsetX >= catSectionWidth * 3 &&
-            e.offsetX <= catSectionWidth * 4 &&
-            e.offsetY >= 0 &&
-            e.offsetY <= catSectionHeight
-        ) {
-            console.log("Bad spot - We're hitting the fourth of the top sections")
-        }
+  catElement.addEventListener("click", (e) => {
+    let catRect = catElement.getBoundingClientRect();
+    let catWidth = catRect.width;
+    let catHeight = catRect.height;
 
+    let catSectionWidth = catWidth / 4;
+    let catSectionHeight = catHeight / 3;
 
-
-
-
-        if (e.offsetX >= 0 &&
-            e.offsetX <= catSectionWidth &&
-            e.offsetY >= catSectionHeight &&
-            e.offsetY <= catSectionHeight * 2
-        ) {
-            console.log("Bad spot - We're hitting the first of the middle sections")
-        }
-
-        if (e.offsetX >= catSectionWidth &&
-            e.offsetX <= catSectionWidth * 2 &&
-            e.offsetY >= catSectionHeight &&
-            e.offsetY <= catSectionHeight * 2
-        ) {
-            console.log("Bad spot - We're hitting the second of the middle sections")
-        }
-
-        if (e.offsetX >= catSectionWidth * 2 &&
-            e.offsetX <= catSectionWidth * 3 &&
-            e.offsetY >= catSectionHeight &&
-            e.offsetY <= catSectionHeight * 2
-        ) {
-            console.log("Good spot - We're hitting the third of the middle sections")
-        }
-
-        if (e.offsetX >= catSectionWidth * 3 &&
-            e.offsetX <= catSectionWidth * 4 &&
-            e.offsetY >= catSectionHeight &&
-            e.offsetY <= catSectionHeight * 2
-        ) {
-            console.log("Bad spot - We're hitting the fourth of the middle sections")
-        }
-
-
-
-
-        if (e.offsetX >= 0 &&
-            e.offsetX <= catSectionWidth &&
-            e.offsetY >= catSectionHeight * 2 &&
-            e.offsetY <= catSectionHeight * 3
-        ) {
-            console.log("Bad spot - We're hitting the first of the bottom sections")
-        }
-
-        if (e.offsetX >= catSectionWidth &&
-            e.offsetX <= catSectionWidth * 3 &&
-            e.offsetY >= catSectionHeight * 2 &&
-            e.offsetY <= catSectionHeight * 3
-        ) {
-            console.log("Bad spot - We're hitting the second of the bottom sections")
-        }
-
-        if (e.offsetX >= catSectionWidth * 2 &&
-            e.offsetX <= catSectionWidth * 3 &&
-            e.offsetY >= catSectionHeight * 2 &&
-            e.offsetY <= catSectionHeight * 3
-        ) {
-            console.log("Bad spot - We're hitting the third of the bottom sections")
-        }
-
-        if (e.offsetX >= catSectionWidth * 3 &&
-            e.offsetX <= catSectionWidth * 4 &&
-            e.offsetY >= catSectionHeight * 2 &&
-            e.offsetY <= catSectionHeight * 3
-        ) {
-            console.log("Bad spot - We're hitting the fourth of the bottom sections")
-        }
-
-
-        // console.log("this is the catRect", catRect)
-
-        // console.log("this is click event object", e)
-    })
-      
-      
+    if (
+      e.offsetX >= 0 &&
+      e.offsetX <= catSectionWidth &&
+      e.offsetY >= 0 &&
+      e.offsetY <= catSectionHeight
+    ) {
+      console.log("Good 1");
+      game.happyCat();
+      game.timer++; /* freezes the timer for 1 second */
     }
-  
-    function restartGame() {
-  
-      startGame()
-  
-      game.gameEndScreen.style.display = 'none'
-      game.gameEndScreen.style.padding = 0
-      game.gameEndScreen.style.height = 0
-  
+
+    if (
+      e.offsetX >= catSectionWidth &&
+      e.offsetX <= catSectionWidth * 2 &&
+      e.offsetY >= 0 &&
+      e.offsetY <= catSectionHeight
+    ) {
+      console.log("Bad 2");
+      game.lives--;
+      game.angryCat();
     }
-  
-    startButton.addEventListener("click", () => {
-        console.log("Clicking Start Button")
-      startGame();
-    });
-  
-    restartButton.addEventListener("click", () => {
-      restartGame();
-    });
-  };
+
+    if (
+      e.offsetX >= catSectionWidth * 2 &&
+      e.offsetX <= catSectionWidth * 3 &&
+      e.offsetY >= 0 &&
+      e.offsetY <= catSectionHeight
+    ) {
+      console.log("Good 3");
+      game.happyCat();
+    }
+
+    if (
+      e.offsetX >= catSectionWidth * 3 &&
+      e.offsetX <= catSectionWidth * 4 &&
+      e.offsetY >= 0 &&
+      e.offsetY <= catSectionHeight
+    ) {
+      console.log("Bad 4");
+      game.lives--;
+      game.angryCat();
+    }
+
+    if (
+      e.offsetX >= 0 &&
+      e.offsetX <= catSectionWidth &&
+      e.offsetY >= catSectionHeight &&
+      e.offsetY <= catSectionHeight * 2
+    ) {
+      console.log("Bad 5");
+      game.lives--;
+      game.angryCat();
+    }
+
+    if (
+      e.offsetX >= catSectionWidth &&
+      e.offsetX <= catSectionWidth * 2 &&
+      e.offsetY >= catSectionHeight &&
+      e.offsetY <= catSectionHeight * 2
+    ) {
+      console.log("Good 6");
+      game.timer++;
+      game.happyCat();
+    }
+
+    if (
+      e.offsetX >= catSectionWidth * 2 &&
+      e.offsetX <= catSectionWidth * 3 &&
+      e.offsetY >= catSectionHeight &&
+      e.offsetY <= catSectionHeight * 2
+    ) {
+      console.log("Good 7");
+      game.happyCat();
+    }
+
+    if (
+      e.offsetX >= catSectionWidth * 3 &&
+      e.offsetX <= catSectionWidth * 4 &&
+      e.offsetY >= catSectionHeight &&
+      e.offsetY <= catSectionHeight * 2
+    ) {
+      console.log("Bad 8");
+      game.lives--;
+      game.angryCat();
+    }
+
+    if (
+      e.offsetX >= 0 &&
+      e.offsetX <= catSectionWidth &&
+      e.offsetY >= catSectionHeight * 2 &&
+      e.offsetY <= catSectionHeight * 3
+    ) {
+      console.log("Bad 9");
+      game.lives--;
+      game.angryCat();
+    }
+
+    if (
+      e.offsetX >= catSectionWidth &&
+      e.offsetX <= catSectionWidth * 2 &&
+      e.offsetY >= catSectionHeight * 2 &&
+      e.offsetY <= catSectionHeight * 3
+    ) {
+      console.log("Bad 10");
+      game.lives--;
+      game.angryCat();
+    }
+
+    if (
+      e.offsetX >= catSectionWidth * 2 &&
+      e.offsetX <= catSectionWidth * 3 &&
+      e.offsetY >= catSectionHeight * 2 &&
+      e.offsetY <= catSectionHeight * 3
+    ) {
+      console.log("super bad spot");
+      /* belly rub = -1 life & -1 second */
+      game.timer--;
+      game.lives--;
+      game.superAngryCat();
+    }
+
+    if (
+      e.offsetX >= catSectionWidth * 3 &&
+      e.offsetX <= catSectionWidth * 4 &&
+      e.offsetY >= catSectionHeight * 2 &&
+      e.offsetY <= catSectionHeight * 3
+    ) {
+      console.log("Bad 11");
+      game.lives--;
+      game.angryCat();
+    }
+  });
+};
